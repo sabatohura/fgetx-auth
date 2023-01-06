d45f092678617fea9b8abf48719f2948ad0b7812
sabatohura <sabato@hura.one>
2023-01-06 14:18:12 +0200

    created login controller

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:fgetx_auth/utils/api_endpoints.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginController extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<void> loginWithEmail() async {
+    var headers = {'Content-Type': 'application/json'};
+    try {
+      var url = Uri.parse(
+          ApiEndPoints.baseUrl + ApiEndPoints.authEndPoints.loginEmail);
+      Map body = {
+        'email': emailController.text.trim(),
+        'password': passwordController.text
+      };
+
+      http.Response response =
+          await http.post(url, body: jsonEncode(body), headers: headers);
+
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        if (json['code'] == 0) {
+          var token = json['data']['token'];
+          final SharedPreferences? prefs = await _prefs;
+          await prefs?.setString('token', token);
+
+          emailController.clear();
+          passwordController.clear();
+          Get.off(HomeScreen());
+        } else if (json['code'] == '1') {
+          throw jsonDecode(response.body)['message'];
+        } else {
+          throw jsonDecode(response.body)['message'] ?? "unknown error";
+        }
+      }
+    } catch (error) {
+      Get.back();
+      showDialog(
+          context: Get.context!,
+          builder: (context) {
+            return SimpleDialog(
+              title: Text('error'),
+              contentPadding: EdgeInsets.all(20),
+              children: [Text(error.toString())],
+            );
+          });
+    }
+  }
+}
