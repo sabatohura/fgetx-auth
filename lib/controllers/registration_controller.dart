ad25dd5cc703b95bbef1a25b804b3e2747f84d8f
sabatohura <sabato@hura.one>
2023-01-06 15:27:55 +0200

    home screen created

--- lib/controllers/registration_controller.dart
+++ lib/controllers/registration_controller.dart
@@ -1,8 +1,5 @@
 import 'dart:convert';
-import 'dart:js';
-
 import 'package:fgetx_auth/utils/api_endpoints.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
