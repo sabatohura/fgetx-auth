eae82523a60c0602fc80500a74388a7469223ab7
sabatohura <sabato@hura.one>
2023-01-06 12:03:44 +0200

    created registration controller

--- lib/controllers/registration_controller.dart
+++ lib/controllers/registration_controller.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:js';
 
 import 'package:fgetx_auth/utils/api_endpoints.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -32,13 +34,27 @@ class RegisterationController extends GetxController {
           var token = json['data']['Token'];
           debugPrint(token);
           final SharedPreferences? prefs = await _prefs;
-
           await prefs?.setString('token', token);
           nameController.clear();
           passwordController.clear();
           emailController.clear();
+        } else {
+          throw jsonDecode(response.body)["Message"] ?? "unknown error Occured";
         }
+      } else {
+        throw jsonDecode(response.body)["Message"] ?? "unknown error Occured";
       }
-    } catch (e) {}
+    } catch (e) {
+      Get.back();
+      showDialog(
+          context: Get.context!,
+          builder: (context) {
+            return SimpleDialog(
+              title: const Text('error'),
+              contentPadding: const EdgeInsets.all(20),
+              children: [Text(e.toString())],
+            );
+          });
+    }
   }
 }
